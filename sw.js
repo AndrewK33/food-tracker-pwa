62c5af44d049ad81aea49d98f7cbe931fb6c6a03
Андрей Коновалов <Andrey.Konovalov@leroymerlin.ru>
2026-05-06 16:09:48 +0300

    modal-bug-fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "food-tracker-pwa-v11";
+const CACHE = "food-tracker-pwa-v13";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
