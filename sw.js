f1038bef54e3d01b3014ceebf76cff60332f9afa
Андрей Коновалов <Andrey.Konovalov@leroymerlin.ru>
2026-05-07 05:51:17 +0300

    fix-bug-action-buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "food-tracker-pwa-v14";
+const CACHE = "food-tracker-pwa-v15";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
